5071cb98fa17928cf8469dd8b4f2dfb240140354
zc1403950372 <1403950372@qq.com>
2025-03-20 15:20:25 +0800

    test

--- src/main/java/com/example/tomatomall/TomatoMallApplication.java
+++ src/main/java/com/example/tomatomall/TomatoMallApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TomatoMallApplication {
-
+//test123
     public static void main(String[] args) {
         SpringApplication.run(TomatoMallApplication.class, args);
     }
